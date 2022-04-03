09e8fd885bdbab3c9314a910dcda1c20a0e30c73
deepakbharti434 <deepakbharati16@gmail.com>
2022-04-03 15:59:35 +0530

    first

---
+++ utility.js
@@ -0,0 +1,8 @@
+let utility ={}
+utility.types = {
+    media: ["mp4", "mkv"],
+   presentation :['pptx'],
+    documents: ['docx', 'doc', 'pdf', 'xlsx', 'xls', 'odt', 'ods', 'odp', 'odg', 'odf', 'txt', 'ps', 'tex'],
+    images:['jpg','jpeg','png']
+}
+module.exports = utility;
